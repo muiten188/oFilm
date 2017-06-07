121c147a93dc2372bd8352dbf963f895e5350e26
bach <buidinhbach123@gmail.com>
2017-06-07 16:55:10 +0700

    orientation

--- src/component/filmdetail/watch_screen.js
+++ src/component/filmdetail/watch_screen.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+var Orientation = require('react-native').NativeModules.Orientation;
+var DeviceEventEmitter = require('react-native').DeviceEventEmitter;
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import FontAwesome, { Icons } from 'react-native-fontawesome';
@@ -19,7 +21,6 @@ import {
   ToastAndroid
 } from 'react-native';
 import FilmPlayer from '../../general_component/filmplayer/ofilmplayer';
-import Orientation from 'react-native-orientation';
 import * as watchScreenActions from "../../actions/component/watch_screen_actions";
 
 class watch extends Component {
@@ -36,44 +37,44 @@ class watch extends Component {
   constructor(props) {
     super(props);
     const { width, height } = Dimensions.get('window');
+    this._fullScreen = true;
     this.state = {
+      _fullScreen: false,
       videoWidth: width,
       videoHeight: height
     }
   }
 
   componentDidMount() {
-    var initial = Orientation.getInitialOrientation();
-    const { width, height } = Dimensions.get('window');
-    if (initial === 'PORTRAIT') {
-      this.reSetWindowSizeState(width, (width * 9) / 16);
-    } else {
-      this.reSetWindowSizeState(width, height);
-    }
-    Orientation.addOrientationListener(this._orientationDidChange);
     //fecth data
     const { episode } = this.props.navigation.state.params;
     const { getLinkFilm } = this.props.watchScreenActions;
     getLinkFilm(episode);
+    DeviceEventEmitter.addListener("orientationDidChange", (body) => this._orientationDidChange(body));
   }
 
-  componentWillUnmount() {
-    Orientation.removeOrientationListener(this._orientationDidChange);
-  }
-
-  _orientationDidChange = (orientation) => {
-    debugger;
-    const { width, height } = Dimensions.get('window');
-    if (orientation === 'LANDSCAPE') {
-      this.reSetWindowSizeState(width, height);
-    } else {
-      this.reSetWindowSizeState(width, (width * 9) / 16);
+  _orientationDidChange(body) {
+    if (body.orientation == "LANDSCAPE" && this.state._fullScreen == false) {
+      this.setState({
+        _fullScreen:true
+      })
+    }
+    else if (body.orientation == "PORTRAIT" && this.state._fullScreen == true) {
+      const { width, height } = Dimensions.get('window');
+      this.setState({
+        _fullScreen:false,
+        videoWidth:width,
+        videoHeight:(width * 9) / 16
+      })
     }
   }
 
-  reSetWindowSizeState(width, height) {
+  componentWillUnmount() {
+    DeviceEventEmitter.removeListener("orientationDidChange");
+  }
+
+  reSetWindowSizeState(height) {
     this.setState({
-      videoWidth: width,
       videoHeight: height
     })
   }
@@ -81,6 +82,18 @@ class watch extends Component {
   navigationBack() {
     const { navigationAction } = this.props.navigation;
     navigationAction.pop();
+  }
+
+  test(event) {
+    let { x, y, width, height } = event.nativeEvent.layout;
+    // if (this._videoWidth != width && this._videoHeight != height && width <= height) {
+    //   this._videoWidth = width;
+    //   this._videoHeight = (width * 9) / 16;
+    //   this.reSetWindowSizeState(this._videoHeight);
+    // }
+    // else if (this._videoWidth != width && this._videoHeight != height && width > height) {
+    //   this.reSetWindowSizeState( "100%");
+    // }
   }
 
   render() {
@@ -98,7 +111,9 @@ class watch extends Component {
       linkFilm = oLinkFilm.link;
     }
     return (
-      <View style={{ width: this.state.videoWidth, height: this.state.videoHeight, flexDirection: 'column' }}>
+      <View style={[this.state._fullScreen ? { flex: 1 } : { width: '100%', height: this.state.videoHeight }, { flexDirection: 'column' }]}
+        onLayout={(event) => this.test(event)}
+      >
         <StatusBar
           hidden={true}
           showHideTransition={'fade'}
